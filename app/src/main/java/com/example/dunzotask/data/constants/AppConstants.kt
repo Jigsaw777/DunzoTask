cf23d5abd9bb81c6da8a5d637755ed90cc4f4c76
Jigsaw <gangulysourik@gmail.com>
2020-08-23 20:33:25 +0530

    spelling error corrected

--- app/src/main/java/com/example/dunzotask/data/constants/AppConstants.kt
+++ app/src/main/java/com/example/dunzotask/data/constants/AppConstants.kt
@@ -4,6 +4,6 @@ object AppConstants{
     const val BASE_URL="https://api.flickr.com/"
     const val GET_IMAGES_ENDPOINT = "services/rest/"
 
-    const val VM_ERROR_MSG="Something went worng. Please try again."
+    const val VM_ERROR_MSG="Something went wrong. Please try again."
     const val NO_NETWORK_ERROR="No internet connection. Please try again later."
 }
